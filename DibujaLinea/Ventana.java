825698392c3429a1f630d6ef4258f41951efed38
Diego Ochoa <dialogo.crg@hotmail.com>
2017-02-27 12:06:18 -0600

    Graphics
    Se agrego el metodo graphics

---
+++ DibujaLinea/Ventana.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class Ventana extends JFrame
+{
+    private Lienzo lienzo;
+    public Ventana(int ancho, int alto)
+    {
+       super.setSize(ancho,alto);
+       super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       lienzo=new Lienzo();
+       super.add(lienzo);
+    }
+    
+}
